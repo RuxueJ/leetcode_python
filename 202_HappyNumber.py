050850dc8e0708b58184d34cbbd5ad5ceb06fb81
RuxueJ <rjin@sfsu.edu>
2023-11-18 23:47:33 -0800

    11/18

--- 202_HappyNumber.py
+++ 202_HappyNumber.py
@@ -4,19 +4,39 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        # record = set()
+        # while n != 1:
+        #     sum = 0
+        #     m = n
+        #     while (n):
+        #         a = n % 10
+        #         sum += a * a
+        #         n /= 10
+        #     if sum in record:
+        #         return False
+        #     else:
+        #         record.add(sum)
+        #         n = sum
+        # return True
+
+
         record = set()
-        while n != 1:
-            sum = 0
-            m = n
-            while (n):
-                a = n % 10
-                sum += a * a
-                n /= 10
-            if sum in record:
-                return False
+        while True:
+            n = self.getSum(n)
+            if n == 1:
+                return True
             else:
-                record.add(sum)
-                n = sum
-        return True
+                if n in record:
+                    return False
+                else:
+                    record.add(n)
+
+    def getSum(self, n):
+        sum = 0
+        while (n):
+            n, r = divmod(n, 10)
+            sum += r * r
+        return sum
+
 
 
