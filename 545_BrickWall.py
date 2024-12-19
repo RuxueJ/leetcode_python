22bc4c306bd61dae14cb1e89e7a593590a6a66fe
RuxueJ <rjin@sfsu.edu>
2024-12-19 13:21:47 -0800

    finish 545 hashmap and array

---
+++ 545_BrickWall.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        n = len(wall)
+        my_dic = defaultdict(int)
+        for rows in wall:
+            base = 0
+            for i in range(len(rows) - 1):
+                base += rows[i]
+                my_dic[base] += 1
+
+        if not my_dic:
+            return len(wall)
+
+        return n - max(my_dic.values())
+
+
