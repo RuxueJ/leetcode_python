bb7706f1ee4772c1b7fcd65c839ce33081d86311
RuxueJ <rjin@sfsu.edu>
2024-12-28 12:15:46 -0800

    12/28

--- 695_Max_Area_Of_Island.py
+++ 695_Max_Area_Of_Island.py
@@ -30,3 +30,32 @@ class Solution(object):
 
         return max_area
 
+    # m, n = len(grid), len(grid[0])
+    # directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+    # visited = set()
+    # max_area = 0
+    #
+    # def bfs(i, j):
+    #     queue = deque([(i, j)])
+    #     visited.add((i, j))
+    #     area = 1
+    #     while queue:
+    #         x, y = queue.popleft()
+    #
+    #         for direction in directions:
+    #             new_x = x + direction[0]
+    #             new_y = y + direction[1]
+    #             if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] == 1 and (new_x, new_y) not in visited:
+    #                 queue.append((new_x, new_y))
+    #                 visited.add((new_x, new_y))
+    #                 area += 1
+    #     return area
+    #
+    # for i in range(m):
+    #     for j in range(n):
+    #         if grid[i][j] == 1 and (i, j) not in visited:
+    #             area = bfs(i, j)
+    #             max_area = max(area, max_area)
+    #
+    # return max_area
+
