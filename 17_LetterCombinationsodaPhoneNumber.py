e5907398cd5ca6cfd7d37494045e83f1af5e9e4a
RuxueJ <rjin@sfsu.edu>
2024-12-30 22:09:05 -0800

    17

--- 17_LetterCombinationsodaPhoneNumber.py
+++ 17_LetterCombinationsodaPhoneNumber.py
@@ -4,29 +4,67 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        map = {
-        '2':'abc',
-        '3':'def',
-        '4':'ghi',
-        '5':'jkl',
-        '6':'mno',
-        '7':'pqrs',
-        '8':'tuv',
-        '9':'wxyz',
-        }
-
-
+        result = []
+        path = []
 
         if not digits:
             return []
 
+        dictionary = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': 'tuv',
+            '9': "wxyz"
+        }
 
-        combinations = [""]
-        for digit in digits:
-            newCom = []
-            for combination in combinations:
-                for letter in map[digit]:
-                    newCom.append(combination+letter)
-            combinations = newCom
+        def backtracking(startIndex):
+            if startIndex == len(digits):
+                result.append(''.join(path))
+                return
 
-        return combinations
+            characters = dictionary[digits[startIndex]]
+
+            for char in characters:
+                path.append(char)
+                backtracking(startIndex + 1)
+                path.pop()
+
+        backtracking(0)
+        return result
+
+# class Solution(object):
+#     def letterCombinations(self, digits):
+#         """
+#         :type digits: str
+#         :rtype: List[str]
+#         """
+#         map = {
+#         '2':'abc',
+#         '3':'def',
+#         '4':'ghi',
+#         '5':'jkl',
+#         '6':'mno',
+#         '7':'pqrs',
+#         '8':'tuv',
+#         '9':'wxyz',
+#         }
+#
+#
+#
+#         if not digits:
+#             return []
+#
+#
+#         combinations = [""]
+#         for digit in digits:
+#             newCom = []
+#             for combination in combinations:
+#                 for letter in map[digit]:
+#                     newCom.append(combination+letter)
+#             combinations = newCom
+#
+#         return combinations
