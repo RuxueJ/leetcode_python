f1a4e2cf5dcfa2cb5186a68e3e410d460d8d0a95
RuxueJ <rjin@sfsu.edu>
2025-01-01 09:58:09 -0800

    151

--- 151_ReverseWordsInAString.py
+++ 151_ReverseWordsInAString.py
@@ -4,16 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        i, j = 0, len(s) - 1
+        """
+        the difference of s.split() and s.split(' ')
+        """
+        word_list = s.split()
+        word_list.reverse()
+        return ' '.join(word_list)
 
-        while i <= j and s[i] == ' ':
-            i += 1
-        while i <= j and s[j] == ' ':
-            j -= 1
-        s = s[i:j + 1]
-        words = s.split()
-        out = []
-        for k in range(len(words) - 1, -1, -1):
-            out.append(words[k])
-        return ' '.join(out)
-
+# class Solution(object):
+#     def reverseWords(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         i, j = 0, len(s) - 1
+#
+#         while i <= j and s[i] == ' ':
+#             i += 1
+#         while i <= j and s[j] == ' ':
+#             j -= 1
+#         s = s[i:j + 1]
+#         words = s.split()
+#         out = []
+#         for k in range(len(words) - 1, -1, -1):
+#             out.append(words[k])
+#         return ' '.join(out)
+#
