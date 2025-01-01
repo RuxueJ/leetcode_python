3ebb7b0a7784d27979125c5e1b03f78a47dc8260
RuxueJ <rjin@sfsu.edu>
2025-01-01 09:47:24 -0800

    345

---
+++ 345_Reverse_Vowerls_of_a_String.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = set("aeiouAEIOU")
+
+        s_list = list(s)
+
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            while i < j and s[i] not in vowels:
+                i += 1
+            while i < j and s[j] not in vowels:
+                j -= 1
+
+            s_list[i], s_list[j] = s_list[j], s_list[i]
+            i += 1
+            j -= 1
+
+        return ''.join(s_list)
