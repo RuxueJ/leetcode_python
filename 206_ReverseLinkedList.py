f3a63d694d1f78f4a727640a430491406c614d2a
RuxueJ <rjin@sfsu.edu>
2025-01-05 09:34:19 -0800

    206

--- 206_ReverseLinkedList.py
+++ 206_ReverseLinkedList.py
@@ -9,25 +9,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # if head == None:
-        #     return None
-        # else:
-        #     current = head
-        #     pre = None
-        #     temp = head.next
-        #     while temp != None:
-        #         current.next = pre
-        #         pre = current
-        #         current = temp
-        #         temp = temp.next
-        #     current.next = pre
-        #     head = current
-        # return head
+
 
         pre = None
         current = head
 
-        while (current):
+        while current:
             temp = current.next
             current.next = pre
             pre = current
