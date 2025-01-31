bb814fbaf5e96cc3178b9a3f39cbd6880ac479d4
RuxueJ <rjin@sfsu.edu>
2025-01-30 22:05:09 -0800

    57

---
+++ 57_Insert_Interval.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        the start is sorted
+        so, find the biggest start < newInterval[0]
+        """
+        result = []
+        i, n = 0, len(intervals)
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        result.append(newInterval)
+
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
