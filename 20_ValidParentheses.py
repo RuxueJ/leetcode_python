a551f130e1c922fc04966bce57974b60ea241b47
RuxueJ <rjin@sfsu.edu>
2025-01-30 22:33:20 -0800

    20

--- 20_ValidParentheses.py
+++ 20_ValidParentheses.py
@@ -4,30 +4,51 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        data = list(s)
         stack = []
-        for parenthese in data:
-            if (parenthese == '(' or parenthese == '[' or parenthese == '{'):
-                stack.append(parenthese)
-            elif len(stack) == 0:
-                return False
-            elif parenthese == ')':
-                if stack[-1] == '(':
-                    stack.pop()
-                else:
-                    return False
-            elif parenthese == ']':
-                if stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
-            else:
-                if stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
-        if len(stack) == 0:
-            return True
-        else:
-            return False
 
+        open_bracket = {'(': ')', '{': '}', '[': ']'}
+
+        for char in s:
+            if char in open_bracket:
+                stack.append(char)
+            else:
+                if not stack or char != open_bracket[stack[-1]]:
+                    return False
+
+                stack.pop()
+
+        return not stack
+
+# class Solution(object):
+#     def isValid(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         data = list(s)
+#         stack = []
+#         for parenthese in data:
+#             if (parenthese == '(' or parenthese == '[' or parenthese == '{'):
+#                 stack.append(parenthese)
+#             elif len(stack) == 0:
+#                 return False
+#             elif parenthese == ')':
+#                 if stack[-1] == '(':
+#                     stack.pop()
+#                 else:
+#                     return False
+#             elif parenthese == ']':
+#                 if stack[-1] == '[':
+#                     stack.pop()
+#                 else:
+#                     return False
+#             else:
+#                 if stack[-1] == '{':
+#                     stack.pop()
+#                 else:
+#                     return False
+#         if len(stack) == 0:
+#             return True
+#         else:
+#             return False
+#
