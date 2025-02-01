adaee405cf294294d15c74447a7fe2ca9a071dd8
RuxueJ <rjin@sfsu.edu>
2025-01-31 16:13:11 -0800

    19

--- 19_RemoveNthNodeFromEndLinkedList.py
+++ 19_RemoveNthNodeFromEndLinkedList.py
@@ -1,38 +1,27 @@
 # Definition for singly-linked list.
-# 快慢双指针
-
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
-        :type head: ListNode
+        :type head: Optional[ListNode]
         :type n: int
-        :rtype: ListNode
+        :rtype: Optional[ListNode]
         """
+        dummy_head = ListNode()
+        dummy_head.next = head
 
-        if (head == None):
-            return None
-
-        dummyNode = ListNode(0, head)
-        first = dummyNode
-        slow = dummyNode
+        fast, slow = dummy_head, dummy_head
 
         for i in range(n):
-            # if(first):
-            first = first.next
-        # else:
-        # return None
-        while (first.next):
-            first = first.next
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
             slow = slow.next
+
         slow.next = slow.next.next
 
-        return dummyNode.next
-
-
-
+        return dummy_head.next
