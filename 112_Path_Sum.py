a1a6c2dbd656679a899f3987369feb7166ec131a
RuxueJ <rjin@sfsu.edu>
2025-02-03 16:05:59 -0800

    112

--- 112_Path_Sum.py
+++ 112_Path_Sum.py
@@ -1,38 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: Optional[TreeNode]
-        :type targetSum: int
-        :rtype: bool
-        """
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        if not root.left and not root.right and root.val == targetSum:
-            return True
 
-        targetSum -= root.val
-        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        def helper(node, target):
+            if not node:
+                return False
+            if not node.left and not node.right:
+                return node.val == target
 
-        # if not root:
-        #     return False
+            return helper(node.left, target - node.val) or helper(node.right, target - node.val)
 
-        # def dfs(node,sum_so_far):
-        #     if not node:
-        #         return False
-
-        #     if not node.left and not node.right:
-        #         return node.val + sum_so_far == targetSum
-
-        #     sum_so_far += node.val
-        #     return dfs(node.left, sum_so_far) or dfs(node.right,sum_so_far)
-
-        # return dfs(root, 0)
-
-
-
+        return helper(root, targetSum)
