e2b4557ad928cb88bbead309408b16312c582898
RuxueJ <rjin@sfsu.edu>
2025-03-28 21:56:31 -0700

    78

--- 78_Subsets.py
+++ 78_Subsets.py
@@ -25,3 +25,15 @@ class Solution(object):
 
         return result
 
+    result = []
+
+        # def backtrack(start, path):
+        #     result.append(path[:])  # Add the current subset (copy to avoid mutation)
+        #     for i in range(start, len(nums)):
+        #         path.append(nums[i])  # Include nums[i]
+        #         backtrack(i + 1, path)  # Recurse with next element
+        #         path.pop()  # Exclude nums[i] (backtrack)
+        #
+        # backtrack(0, [])
+        # return result
+
